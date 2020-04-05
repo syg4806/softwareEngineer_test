8855eb33d9c92aeba2efa1f5bbbd0b74409a2589
Kwon <syg4806@naver.com>
2020-04-05 19:27:08 +0900

    I'm master

--- TestCode1.c
+++ TestCode1.c
@@ -4,5 +4,11 @@ void main(){
 	int a=3;
 	printf("%d",&a);
 	return;
-	//난 otherComputer라고해
+}
+void test(){
+	//I wanna make test() like this by master
+}
+
+void test2(){
+	//I wanna make test2() like this by master
 }
